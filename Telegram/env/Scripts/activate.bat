ead91ca878b0fb2698dce26635594fe3c079c3dc
Liam Wells Ayathan <ayathan21@gmail.com>
2023-11-11 13:36:38 +0800

    updated virtual env

--- Telegram/env/Scripts/activate.bat
+++ Telegram/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Telegram bots\Onlyloans\env
+set VIRTUAL_ENV=D:\Telegram bots\Onlyloans\Telegram\env
 
 if not defined PROMPT set PROMPT=$P$G
 
